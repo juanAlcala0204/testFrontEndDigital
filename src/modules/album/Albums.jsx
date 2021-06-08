15c2c0a9485a9145fab3c7828117f8e4145981f8
juanAlcala0204 <juan.alcala0204@gmail.com>
2021-06-08 02:11:36 -0500

    refactory/Module Albums - create basic Structure

--- src/modules/album/Albums.jsx
+++ src/modules/album/Albums.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import BasicCard from '../../components/BasicCard'
+
+import '../../assets/css/Albums/albums.css'
+
 export default function Albums() {
     return (
         <div>
-            
-        </div>
+        <BasicCard>
+            <div className='row'>
+                <div className='col'>
+                    <nav class="navbar navbar-light bg-light navbarAlbums">
+                        <div className='col'></div>
+                        <div className='col'>
+                            <a class="navbar-brand" href="#">Albumes</a>
+                        </div>
+                        <div className='col'></div>
+                    </nav>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col'>
+                    <BasicCard>
+                       
+                    </BasicCard>
+                </div>
+            </div>
+        </BasicCard>
+    </div>
     )
 }
